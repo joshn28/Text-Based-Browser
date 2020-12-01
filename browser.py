b87249084f5452999f31341370085708f087d1e6
joshn28 <joshnobleza@bell.net>
2020-12-01 15:17:20 -0500

    Make a beautifulsoup object to parse the webpage

--- browser.py
+++ browser.py
@@ -1,40 +1,7 @@
 import os
 import requests
+from bs4 import BeautifulSoup
 
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
 
 # write your code here
 
@@ -70,10 +37,18 @@ while True:
                 stack.append(current_page)
                 current_page = user_input
             r = requests.get(user_input)
-            with open("./{}/".format(folder) + user_input[:len(user_input)-4].lstrip("https://").lstrip("www."), "w") \
-                    as f:
-                print(r.text)
-                f.write(r.text)
+            if r:
+                soup = BeautifulSoup(r.content, 'html.parser')
+                tags = ['p', 'a', 'ul', 'ol', 'li']
+                web_page = []
+                for tag in tags:
+                    web_page.extend(soup.find_all(tag))
+                websiteName = user_input[:len(user_input) - 4].lstrip("https://").lstrip("www.")
+                with open("./{}/".format(folder) + websiteName, "w") \
+                        as f:
+                    for el in web_page:
+                        print(el.text)
+                        f.write(el.text)
         else:
             print("Error: Unknown URL")
     elif user_input == "back":
