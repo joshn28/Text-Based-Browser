e38df85ed9acd679c051b8f3e3e2bc7f3e57ea44
joshn28 <joshnobleza@bell.net>
2020-12-09 21:31:33 -0500

    A function that writes the contents of a webpage to a file

--- browser.py
+++ browser.py
@@ -18,18 +18,43 @@ def create_new_folder(name="tb_tabs"):
 
 def check_valid_domain_name(website):
     """
+    Checks if the domain name given by the user is valid
     :param website: website name
     :return: true if website name matches with regular expression, else false
     """
     return re.search("(https:\/\/)*www.*\..*\.\w{2,}", website)
 
 
+def create_webpage_file(domain_name):
+    """
+    Creates a new file named after the domain name and writes content from certain html tags into the file
+    :param domain_name: domain name
+    """
+    r = requests.get(domain_name)
+    if r:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        tags = ['p', 'a', 'ul', 'ol', 'li']
+        web_page = []
+        for tag in tags:
+            web_page.extend(soup.find_all(tag))
+        website_name = domain_name[:len(domain_name) - 4].lstrip("https://").lstrip("www.")
+        with open("./{}/".format(folder) + website_name, "w") \
+                as f:
+            for el in web_page:
+                if el.name == 'a':
+                    print(Fore.BLUE + el.text)
+                else:
+                    print(el.text)
+                f.write(el.text)
+    else:
+        print("Error: Unknown URL")
+
+
 stack = []
 current_page = ""
 
 while True:
-    user_input = input()
-    folder = "tb_tabs"
+    user_input = input("Enter a URL or make a new directory for the webpage: ")
     if "dir" in user_input:
         create_new_folder(user_input.split()[1])
         folder = user_input.split()[1]
@@ -44,24 +69,7 @@ while True:
         else:
             stack.append(current_page)
             current_page = user_input
-        r = requests.get(user_input)
-        if r:
-            soup = BeautifulSoup(r.content, 'html.parser')
-            tags = ['p', 'a', 'ul', 'ol', 'li']
-            web_page = []
-            for tag in tags:
-                web_page.extend(soup.find_all(tag))
-            websiteName = user_input[:len(user_input) - 4].lstrip("https://").lstrip("www.")
-            with open("./{}/".format(folder) + websiteName, "w") \
-                    as f:
-                for el in web_page:
-                    if el.name == 'a':
-                        print(Fore.BLUE + el.text)
-                    else:
-                        print(el.text)
-                    f.write(el.text)
-        else:
-            print("Error: Unknown URL")
+        create_webpage_file(user_input)
     elif user_input == "back":
         if len(stack) > 0:
             print(stack.pop())
